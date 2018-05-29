bc21f049368af8a896093852c5ffad345f3dc590
Blake Song <Blake.Song@smith.co>
2018-05-29 16:44:52 +0800

    add breadcrumb

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,25 +1,40 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom'
+     
+import Breadcrumbs  from 'react-router-dynamic-breadcrumbs';   
+  
+/**
+*  Create routes mapping
+*  
+*  All dynamic params will display automatically.
+*  not that even though '/users/:id' route is not in configuration file, 
+*  it's corresponding link it will be displayed as the value of ':id'
+*/
+const routes = {
+	'/': '首页',
+	'/category': '全部商品',
+   
+};
+	
+// https://www.npmjs.com/package/react-router-dynamic-breadcrumbs
 
-export default class breadcrumb extends PureComponent {
-  render(){
+export default class Breadcrumb extends Component {
+
+  render() {
+	
     return (
 			<div className="container">
 				<div id="breadcrumb" className="breadcrumb">
-						<ul className="clearfix">
-								<li>
-										<a href="/">首页</a>
-								</li>
 
-								<li className="separator">&gt;</li>
-								<li>
-										<a className="last">登录</a>
-								</li>
-
-						</ul>
+						<Router>
+								<Breadcrumbs mappedRoutes={routes} />
+						</Router>
+					
 				</div>
 			</div>
-    )
+    );
   }
-
 }
-
+ 
